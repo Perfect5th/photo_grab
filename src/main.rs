af934d4248b5be5220bb3efe545555f2a0ae6f2e
perfect5th <mitch.b45@gmail.com>
2016-11-05 15:58:30 -0700

    semi-functional

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    match fs::create_dir("grabbed_photos") {
+        Ok(res) => res,
+        Err(e) => println!("Something went wrong: {}", e),
+    }
+    fs::copy("photos/1.jpg", "photos/1.jpg");
 }
