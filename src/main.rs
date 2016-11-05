15c66fb79d0f97eb5e5a5ceabbde5b425ec9d4ae
perfect5th <mitch.b45@gmail.com>
2016-11-05 16:07:33 -0700

    Add photo copy

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ use std::fs;
 fn main() {
     match fs::create_dir("grabbed_photos") {
         Ok(res) => res,
-        Err(e) => println!("Something went wrong: {}", e),
+        Err(e) => println!("Something went wrong during directory creation: {:?}", e),
     }
-    fs::copy("photos/1.jpg", "grabbed_photos/1.jpg");
+    match fs::copy("photos/1.jpg", "grabbed_photos/1.jpg") {
+        Ok(res) => println!("Results was: {}", res),
+        Err(e) => println!("Something went wrong during file copy: {:?}", e),
+    }
 }
