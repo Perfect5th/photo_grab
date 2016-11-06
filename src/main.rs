ea40ca6eb38818257029b5a80123ee02102a75b9
perfect5th <mitch.b45@gmail.com>
2016-11-05 17:04:09 -0700

    working 1 level deep

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
 use std::fs;
+use std::path::PathBuf;
 
 fn main() {
+    let contents = fs::read_dir("photos").unwrap();
+
     match fs::create_dir("grabbed_photos") {
         Ok(res) => res,
-        Err(e) => println!("Something went wrong during directory creation: {:?}", e),
+        Err(e) => println!("Something went wrong during directory creation: {}", e),
     }
-    match fs::copy("photos/1.jpg", "grabbed_photos/1.jpg") {
-        Ok(res) => println!("Results was: {}", res),
-        Err(e) => println!("Something went wrong during file copy: {:?}", e),
+
+    for file in contents {
+        let file_path = file.unwrap().path();
+        let file_name = file_path.file_name().unwrap();
+        println!("Copying file: {:?}", file_path);
+        let mut dest_path = PathBuf::new();
+        dest_path.push("grabbed_photos");
+        dest_path.push(file_name);
+        println!("path to copy from {:?}", file_path.as_path());
+        println!("path to copy to {:?}", dest_path.as_path());
+        match fs::copy(file_path.as_path(), dest_path) {
+            Ok(_) => println!("Successfully copied"),
+            Err(e) => panic!("Something went wrong during copy: {}", e),
+        }
     }
 }
