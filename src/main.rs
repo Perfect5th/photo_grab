44292859c68d84ea70c41bf2aee02749b464a67b
perfect5th <mitch.b45@gmail.com>
2016-11-06 18:43:49 -0800

    Add comments

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,19 @@ use std::fs;
 use std::path::PathBuf;
 
 fn main() {
+    // read target directory.
+    // TODO: make recursive, handle errors (own function), use user-given target.
     let contents = fs::read_dir("photos").unwrap();
 
+    // create directory for dump.
+    // TODO: handle error if dir exists, use existing dir, use user-given target.
     match fs::create_dir("grabbed_photos") {
         Ok(res) => res,
         Err(e) => println!("Something went wrong during directory creation: {}", e),
     }
 
+    // iterate over files in target dir, copy to dest dir.
+    // TODO: handle errors in file_path
     for file in contents {
         let file_path = file.unwrap().path();
         let file_name = file_path.file_name().unwrap();
